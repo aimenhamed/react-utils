08c9e807047e1c95ce04faaf3d546f7b9cf347b2
Aimen Hamed <aimenhamed@live.com.au>
2022-11-25 23:57:08 +1100

    allowing generic for response data

--- src/helpers/request/request.ts
+++ src/helpers/request/request.ts
@@ -8,11 +8,11 @@ const joinOptions = (options: (Options | undefined)[]): Options => {
   };
 };
 
-export const sendRequest = async (
+export const sendRequest = async <T>(
   url: string,
   requestType: RequestType,
   options?: Options
-): Promise<unknown> => {
+): Promise<T> => {
   const defaultOptions: Options = {
     timeout: 10000,
     method: requestType.toUpperCase(),
@@ -22,6 +22,9 @@ export const sendRequest = async (
     },
   };
 
-  const response = await ky(url, joinOptions([defaultOptions, options])).json();
+  const response = (await ky(
+    url,
+    joinOptions([defaultOptions, options])
+  ).json()) as T;
   return response;
 };
